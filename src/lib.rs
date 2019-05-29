da771f6f4e55886980b758261af1c47b7d29804e
Matthieu Le Brazidec <matthieu@lebrazidec.email>
2019-05-29 16:25:58 +0200

    Updated the main crate exports.

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,8 @@ pub use aktoro_raw as raw;
 pub use aktoro_runtime::*;
 
 pub mod prelude {
-    pub use aktoro_raw::Action as AkAction;
-    pub use aktoro_raw::Actor as AkActor;
     pub use aktoro_raw::Context as AkContext;
-    pub use aktoro_raw::Controlled as AkControlled;
-    pub use aktoro_raw::Event as AkEvent;
-    pub use aktoro_raw::Handler as AkHandler;
-    pub use aktoro_raw::Message as AkMessage;
-    pub use aktoro_raw::Receiver as AkReceiver;
+    pub use aktoro_raw::Controller as AkController;
     pub use aktoro_raw::Runtime as AkRuntime;
     pub use aktoro_raw::Sender as AkSender;
-    pub use aktoro_raw::Status as AkStatus;
-    pub use aktoro_raw::Update as AkUpdate;
-    pub use aktoro_raw::Work as AkWork;
 }
