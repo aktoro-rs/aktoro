12e454ef2c621566310f537b88d5068c2f936433
Matthieu Le Brazidec <matthieu@lebrazidec.email>
2019-05-29 16:26:13 +0200

    Updated the example.

--- examples/hello_world/src/main.rs
+++ examples/hello_world/src/main.rs
@@ -1,20 +1,21 @@
 #![feature(async_await)]
 
 use aktoro::prelude::*;
+use aktoro::raw::ActionHandler;
 use aktoro::raw::Actor;
 use aktoro::raw::Handler;
 use aktoro::*;
 
-pub struct HelloActor;
+struct HelloActor;
 
-pub struct Hello(&'static str);
+struct Hello(&'static str);
+
+struct Kill;
 
 impl Actor for HelloActor {
     type Context = Context<Self>;
 
-    type Action = ();
-    type Event = ();
-    type Status = bool;
+    type Status = ();
 }
 
 impl Handler<Hello> for HelloActor {
@@ -25,11 +26,35 @@ impl Handler<Hello> for HelloActor {
     }
 }
 
+impl ActionHandler<Kill> for HelloActor {
+    fn handle(&mut self, _: Kill, ctx: &mut Context<Self>) {
+        ctx.kill();
+    }
+}
+
 #[runtime::main]
 async fn main() {
-    let mut rt = Runtime;
+    let mut rt = Runtime::init();
 
-    let (_, mut sender) = rt.spawn(HelloActor);
+    let (ctrler, sender, _) = rt.spawn(HelloActor);
 
-    println!("{:?}", sender.send(Hello("World")).unwrap().await.unwrap());
+    runtime::spawn(run("World", ctrler, sender));
+
+    rt.wait().await.unwrap();
+}
+
+async fn run(
+    hello: &'static str,
+    mut ctrler: Controller<HelloActor>,
+    mut sender: Sender<HelloActor>,
+) {
+    let msg = Hello(hello);
+
+    let req = sender.send(msg).unwrap();
+    let resp = req.await.unwrap();
+
+    println!("{}", resp);
+
+    let req = ctrler.send(Kill).unwrap();
+    req.await.unwrap();
 }
