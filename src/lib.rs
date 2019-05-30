c796bd879619bad042e47fd4eb5f5a5921d1aefc
Matthieu Le Brazidec <matthieu@lebrazidec.email>
2019-05-30 08:24:56 +0200

    Updated the main crate.

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,13 @@ pub use aktoro_raw as raw;
 pub use aktoro_runtime::*;
 
 pub mod prelude {
+    pub use aktoro_raw::ActionHandler;
+    pub use aktoro_raw::Actor;
     pub use aktoro_raw::Context as AkContext;
     pub use aktoro_raw::Controller as AkController;
+    pub use aktoro_raw::EventHandler;
+    pub use aktoro_raw::Handler;
     pub use aktoro_raw::Runtime as AkRuntime;
     pub use aktoro_raw::Sender as AkSender;
+    pub use aktoro_raw::Status as AkStatus;
 }
