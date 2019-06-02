5e4f39c7a51c0eef0bd9bbb9cbb9a4229ceeff28
Matthieu Le Brazidec <matthieu@lebrazidec.email>
2019-06-02 23:19:04 +0200

    Updated the main crate.

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,32 @@
 pub use aktoro_raw as raw;
 
-#[cfg(feature = "aktoro-runtime")]
-pub use aktoro_runtime::*;
+#[cfg(feature = "context")]
+pub use aktoro_context as context;
+
+#[cfg(feature = "runtime")]
+pub use aktoro_runtime as runtime;
 
 pub mod prelude {
-    pub use aktoro_raw::ActionHandler;
     pub use aktoro_raw::Actor;
-    pub use aktoro_raw::Context as AkContext;
-    pub use aktoro_raw::Controller as AkController;
+    pub use aktoro_raw::Context as RawContext;
+
+    pub use aktoro_raw::ActionHandler;
     pub use aktoro_raw::EventHandler;
     pub use aktoro_raw::Handler;
-    pub use aktoro_raw::Runtime as AkRuntime;
-    pub use aktoro_raw::Sender as AkSender;
-    pub use aktoro_raw::Status as AkStatus;
+
+    pub use aktoro_raw::Runtime as RawRuntime;
+    pub use aktoro_raw::Spawned;
+
+    pub use aktoro_raw::Receiver as RawReceiver;
+    pub use aktoro_raw::Sender as RawSender;
+    pub use aktoro_raw::Controlled as RawControlled;
+    pub use aktoro_raw::Controller as RawController;
+    pub use aktoro_raw::Updated as RawUpdated;
+    pub use aktoro_raw::Updater as RawUpdater;
+
+    #[cfg(feature = "context")]
+    pub use crate::context::*;
+
+    #[cfg(feature = "runtime")]
+    pub use crate::runtime::*;
 }
