5d193fa198af9ed86121f9a2eb889987389079a0
Matthieu Le Brazidec <matthieu@lebrazidec.email>
2019-07-10 19:36:04 +0200

    Updated the network example.

--- examples/net/src/main.rs
+++ examples/net/src/main.rs
@@ -1,17 +1,40 @@
 #![feature(async_await)]
 
-use std::task::Poll;
-
 use aktoro::prelude::*;
 use aktoro::raw;
-use futures_util::poll;
+use futures_util::io::AsyncReadExt;
+use futures_util::io::WriteHalf;
+
+struct Client<C: raw::TcpClient> {
+    connect: Option<C::Connect>,
+    write: Option<WriteHalf<C>>,
+}
 
 struct Server<S: raw::TcpServer> {
     tcp: Option<S>,
 }
 
-struct Connection<S: raw::TcpServer> {
-    stream: S::Stream,
+struct Connected<C: raw::TcpClient>(C);
+
+struct Connection<S: raw::TcpServer>(S::Stream);
+
+struct Sent<C: raw::TcpClient>(WriteHalf<C>);
+
+struct Received(Vec<u8>);
+
+impl<C> Actor for Client<C>
+where
+    C: raw::TcpClient + 'static,
+{
+    type Context = Context<Self>;
+    type Status = Status;
+    type Error = Error;
+
+    fn started(&mut self, ctx: &mut Self::Context) {
+        let connect = self.connect.take().unwrap();
+
+        ctx.wait(connect, |client| Connected(client.unwrap()));
+    }
 }
 
 impl<S> Actor for Server<S>
@@ -23,12 +46,29 @@ where
     type Error = Error;
 
     fn started(&mut self, ctx: &mut Self::Context) {
-        println!("started");
-        ctx.subscribe(self.tcp.take().unwrap().into_incoming().unwrap(), |conn| {
-            Connection {
-                stream: conn.unwrap(),
-            }
+        let tcp = self.tcp.take().unwrap();
+        println!("listening on {}", tcp.local_addr().unwrap());
+
+        ctx.subscribe(tcp.into_incoming().unwrap(), |conn| {
+            Connection(conn.unwrap())
         });
+    }
+}
+
+impl<C> Handler<Connected<C>> for Client<C>
+where
+    C: raw::TcpClient + 'static,
+{
+    type Output = ();
+
+    fn handle(&mut self, msg: Connected<C>, ctx: &mut Self::Context) -> Result<(), Self::Error> {
+        let client = msg.0;
+        println!("connected to {}", client.peer_addr().unwrap());
+
+        let (_, write) = client.split();
+        ctx.write(write, vec![0], |_, write| Sent(write), |_| ());
+
+        Ok(())
     }
 }
 
@@ -38,8 +78,44 @@ where
 {
     type Output = ();
 
-    fn handle(&mut self, msg: Connection<S>, ctx: &mut Context<Self>) -> Result<(), Self::Error> {
-        println!("new connection");
+    fn handle(&mut self, msg: Connection<S>, ctx: &mut Self::Context) -> Result<(), Self::Error> {
+        let conn = msg.0;
+        println!("new connection from {}", conn.peer_addr().unwrap());
+
+        let (read, _) = conn.split();
+        ctx.read(read, 64, |data| Received(data), |_| ());
+
+        Ok(())
+    }
+}
+
+impl<C> Handler<Sent<C>> for Client<C>
+where
+    C: raw::TcpClient + 'static,
+{
+    type Output = ();
+
+    fn handle(&mut self, sent: Sent<C>, ctx: &mut Self::Context) -> Result<(), Self::Error> {
+        println!("sent data");
+        self.write = Some(sent.0);
+
+        ctx.set_status(Status::Stopped);
+
+        Ok(())
+    }
+}
+
+impl<S> Handler<Received> for Server<S>
+where
+    S: raw::TcpServer + 'static,
+{
+    type Output = ();
+
+    fn handle(&mut self, msg: Received, ctx: &mut Self::Context) -> Result<(), Self::Error> {
+        println!("received {:?}", msg.0);
+
+        ctx.set_status(Status::Stopped);
+
         Ok(())
     }
 }
@@ -55,21 +131,14 @@ async fn main() {
 
     rt.spawn(server).unwrap();
 
-    let mut run = runtime::spawn(run(net));
-    let mut wait = rt.wait();
+    let client = Client::<TcpClient> {
+        connect: Some(net.tcp_connect("127.0.0.1:5555").unwrap()),
+        write: None,
+    };
 
-    loop {
-        if let Poll::Ready(_) = poll!(&mut run) {
-            break;
-        }
+    rt.spawn(client).unwrap();
 
-        if let Poll::Ready(res) = poll!(&mut wait) {
-            res.expect("an error occured while waiting for the runtime to stop");
-            break;
-        }
-    }
-}
-
-async fn run<N: raw::NetworkManager>(net: N) {
-    loop {}
+    rt.wait()
+        .await
+        .expect("an error occured while waiting for the runtime to stop");
 }
